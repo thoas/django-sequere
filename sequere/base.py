4dd6735cfd535ba8291e9ad4963e1708de25b2da
Florent Messa <florent.messa@gmail.com>
2013-10-25 12:55:07 +0200

    No need for slugify in ModelBase.get_identifier

--- sequere/base.py
+++ sequere/base.py
@@ -1,9 +1,6 @@
-from django.template.defaultfilters import slugify
-
-
 class ModelBase(object):
     model = None
     identifier = None
 
     def get_identifier(self):
-        return self.identifier or slugify(self.model.__name__)
+        return self.identifier or self.model.__name__.lower()
