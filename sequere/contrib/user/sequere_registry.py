8a9c185b18ad6e0b498366e2fac190bac8dde98e
Florent Messa <florent.messa@gmail.com>
2013-11-19 16:19:20 +0100

    Add missing register

--- sequere/contrib/user/sequere_registry.py
+++ sequere/contrib/user/sequere_registry.py
@@ -1,7 +1,11 @@
 from sequere.compat import User
+from sequere.registry import registry
 from sequere.base import ModelBase
 
 
 class UserSequere(ModelBase):
     model = User
     identifier = 'user'
+
+
+registry.register(User, UserSequere)
