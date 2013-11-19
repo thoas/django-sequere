d8e872c3d2aa141c29d993c08c207c1b7994b055
Florent Messa <florent.messa@gmail.com>
2013-11-19 15:49:48 +0100

    Add missing filter decorators

--- sequere/templatetags/sequere_tags.py
+++ sequere/templatetags/sequere_tags.py
@@ -6,13 +6,16 @@ from sequere.models import (get_followers_count, get_followings_count)
 register = template.Library()
 
 
+@register.filter
 def identifier(instance, arg=None):
     return registry.get_identifier(instance)
 
 
+@register.filter
 def followers_count(instance, identifier=None):
     return get_followers_count(instance, identifier)
 
 
+@register.filter
 def followings_count(instance, identifier=None):
     return get_followings_count(instance, identifier)
