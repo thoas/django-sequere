8a860b46ad8b4ff05561a5219d66bf3ab16ab22a
Florent Messa <florent.messa@gmail.com>
2013-11-27 17:18:56 +0100

    Fix RedisFallbackBackend asynchronous tasks

--- sequere/backends/redis/tasks.py
+++ sequere/backends/redis/tasks.py
@@ -1,13 +1,17 @@
 from celery.task import task
 
+from sequere.utils import load_class
+
 
 @task
 def follow(backend_class,
            from_instance_class, from_instance_pk,
            to_instance_class, to_instance_pk):
 
-    return backend_class().follow(from_instance_class.objects.get(pk=from_instance_pk),
-                                  to_instance_class.objects.get(pk=to_instance_pk))
+    backend = load_class(backend_class)()
+
+    return backend.follow(from_instance_class.objects.get(pk=from_instance_pk),
+                          to_instance_class.objects.get(pk=to_instance_pk))
 
 
 @task
@@ -15,5 +19,7 @@ def unfollow(backend_class,
              from_instance_class, from_instance_pk,
              to_instance_class, to_instance_pk):
 
-    return backend_class().unfollow(from_instance_class.objects.get(pk=from_instance_pk),
-                                    to_instance_class.objects.get(pk=to_instance_pk))
+    backend = load_class(backend_class)()
+
+    return backend.unfollow(from_instance_class.objects.get(pk=from_instance_pk),
+                            to_instance_class.objects.get(pk=to_instance_pk))
