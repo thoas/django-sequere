3f91545d0204470c822f1e1b6c1b3bf705c8478c
Florent Messa <florent.messa@gmail.com>
2014-08-12 14:57:56 +0200

    Remove use_caching stuffs on signals

--- sequere/backends/redis/signals.py
+++ sequere/backends/redis/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import Signal
 
-follow = Signal(providing_args=['from_instance', 'to_instance', ], use_caching=True)
+follow = Signal(providing_args=['from_instance', 'to_instance', ])
 
-unfollow = Signal(providing_args=['from_instance', 'to_instance', ], use_caching=True)
+unfollow = Signal(providing_args=['from_instance', 'to_instance', ])
--- sequere/contrib/timeline/signals.py
+++ sequere/contrib/timeline/signals.py
@@ -1,4 +1,4 @@
 from django.dispatch import Signal
 
-pre_save = Signal(providing_args=['instance', 'action', ], use_caching=True)
-post_save = Signal(providing_args=['instance', 'action', ], use_caching=True)
+pre_save = Signal(providing_args=['instance', 'action', ])
+post_save = Signal(providing_args=['instance', 'action', ])
